2acc8e84f3671b0163dde1b4acf0e391c4050003
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-03-03 12:26:40 +0000

    Hard-code the returned pixel colour to 191 red

--- main.lua
+++ main.lua
@@ -1,8 +1,10 @@
 function love.load()
     myShader = love.graphics.newShader[[
         vec4 effect( vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords ){
-            vec4 pixel = Texel(texture, texture_coords);//This is the current pixel color
-            return pixel * color;
+            //This is the current pixel color
+            vec4 pixel = Texel(texture, texture_coords);
+            //return pixel * color;
+            return vec4(0.75, 0.0, 0.0, 1.0);
         }
     ]]
 end
