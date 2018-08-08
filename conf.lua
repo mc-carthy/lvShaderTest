e8414335d363e782199da4cca5d318e5f42f7a2d
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-08-08 23:34:13 +0100

    Draw gradient over image

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
-    t.window.width = 800
-    t.window.height = 600
+    t.window.width = 900
+    t.window.height = 477
     t.window.title = 'LÖVE Shader Test'
     t.window.icon = nil
 
--- main.lua
+++ main.lua
@@ -1,12 +1,11 @@
 local shaderCode = [[
+    extern vec2 screen;
     vec4 effect(vec4 colour, Image image, vec2 uvs, vec2 screen_coords) {
         vec4 pixel = Texel(image, uvs);
 
-        float av = (pixel.r + pixel.g + pixel.b) / 3;
-        
-        //return vec4(1.0, 0.0, 0.0, 1.0);
-        //return pixel * colour;
-        return vec4(av, av, av, 1.0) * colour;
+        vec2 sc = screen_coords.xy / screen;
+
+        return vec4(sc, 1.0, 1.0) * pixel;
     }
 ]]
 
@@ -18,7 +17,9 @@ end
 function love.draw()
     love.graphics.setShader(shader)
     -- love.graphics.setColor(1.0, 0.0, 0.0, 1.0)
+    shader:send("screen", { love.graphics.getDimensions() })
     love.graphics.draw(image, 0, 0)
+    -- love.graphics.rectangle('fill', 0, 0, love.graphics.getDimensions())
     love.graphics.setShader()
 end
 
